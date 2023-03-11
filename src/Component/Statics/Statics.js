bf0c3cb13fc74ddcf51b931a6aa9c1132ab73a12
Debos <debos.das.02@gmail.com>
2023-03-12 02:20:10 +0600

    added Linechart

--- src/Component/Statics/Statics.js
+++ src/Component/Statics/Statics.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import { LineChart,Line,
+    XAxis,
+    YAxis,
+    CartesianGrid,
+    Tooltip,
+    Legendgit } from 'recharts';
 
 const Statics = () => {
     const {total}=useLoaderData();
     
     return (
-        <div>
-            <h2>This is static component</h2>
+        <div className='container text-white'>
+            <h1 className='my-5'>Rechart</h1>
+            <LineChart></LineChart>
         </div>
     );
 };
