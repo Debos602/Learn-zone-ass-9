b4cff8b15601df72820c379fc9701301789b35cb
Debos <debos.das.02@gmail.com>
2023-03-12 03:50:20 +0600

    updated nav

--- src/Component/NavBar/NavBar.js
+++ src/Component/NavBar/NavBar.js
@@ -4,7 +4,6 @@ import { Link, NavLink } from 'react-router-dom';
 import './NavBar.css'
 
 const NavBar = () => {
-  const []
     return (
         <div className='bg-color'>
             <nav className='nav-bar container py-2'>
